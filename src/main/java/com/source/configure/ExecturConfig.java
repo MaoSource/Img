1e3c29a60f5493800503f3698086bb23c77e9b9d
Source <51420@sohu.com>
2022-02-28 16:03:56 +0800

    添加异步多线程

---
+++ src/main/java/com/source/configure/ExecturConfig.java
@@ -0,0 +1,31 @@
+package com.source.configure;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author Source
+ * @date 2021/11/02/14:56
+ */
+@Configuration
+public class ExecturConfig {
+    @Bean("taskExector")
+    public Executor taskExector() {
+
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        int i = Runtime.getRuntime().availableProcessors();//获取到服务器的cpu内核
+        executor.setCorePoolSize(5);//核心池大小
+        executor.setMaxPoolSize(100);//最大线程数
+        executor.setQueueCapacity(1000);//队列程度
+        executor.setKeepAliveSeconds(1000);//线程空闲时间
+        executor.setThreadNamePrefix("tsak-asyn");//线程前缀名称
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.AbortPolicy());//配置拒绝策略
+        return executor;
+    }
+}
